dbf3fbdc2d920e73f7cd74bdc33d4d03c31922e5
Timethethief <3153737813@qq.com>
2022-02-23 15:14:29 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @author Timethethief  Email:3153737813@qq.com
+ * @Description
+ * @date 2022/2/23 15:03
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+
+}
